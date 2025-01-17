ef1bf16b36e436686316c39739811161399e7797
wsameer <dev.wsameer@gmail.com>
2025-01-16 21:59:39 -0500

    adds events handler for creating default categories

--- app/listeners/create_cash_account.ts
+++ app/listeners/create_cash_account.ts
@@ -1,23 +1,26 @@
-import Account from '#models/account'
-import { AccountType } from '../types/accounts.js'
 import { Exception } from '@adonisjs/core/exceptions'
-import UserRegistered from '#events/user_registered'
+
+import Account from '#models/account'
+import type UserRegistered from '#events/user_registered'
+import { AccountType } from '../types/accounts.js'
+import logger from '@adonisjs/core/services/logger'
 
 class CashAccountCreationException extends Exception {
   static status = 500
 }
 
 export default class CreateCashAccount {
-  async handle(userRegistered: UserRegistered) {
+  async handle(event: UserRegistered) {
     try {
       await Account.create({
         name: 'Cash',
-        userId: userRegistered.user.id,
+        userId: event.user.id,
         group: AccountType[0],
         balance: 0.0,
         paymentAccountId: null,
         description: 'Everyone start with a cash account',
       })
+      logger.log('info', `[CreateCashAccount] Created cash account for user ${event.user.email}`)
     } catch (error) {
       console.log(error)
       throw new CashAccountCreationException('Failed to create cash account', { cause: error })
--- start/events.ts
+++ start/events.ts
@@ -2,9 +2,12 @@ import emitter from '@adonisjs/core/services/emitter'
 import logger from '@adonisjs/core/services/logger'
 
 import UserRegistered from '#events/user_registered'
-const CreateCashAccount = () => import('#listeners/create_cash_account')
 
-emitter.on(UserRegistered, [CreateCashAccount])
+const CreateExpenseCategories = () => import('#listeners/create_expense_categories')
+const CreateCashAccount = () => import('#listeners/create_cash_account')
+const CreateIncomeCategories = () => import('#listeners/create_income_categories')
+
+emitter.listen(UserRegistered, [CreateCashAccount, CreateExpenseCategories, CreateIncomeCategories])
 
 emitter.onError((event, error, eventData) => {
   logger.error('Event handling failed:', {
