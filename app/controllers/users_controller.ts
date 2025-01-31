a6111618d8071034b5f7bddd36f6143d9ea651a3
wsameer <dev.wsameer@gmail.com>
2025-01-31 12:14:29 -0400

    env variable name changes for server

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,8 +1,9 @@
+import User from '#models/user'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class UsersController {
   async index({ auth }: HttpContext) {
     await auth.authenticate()
-    return auth.user
+    return User.all()
   }
 }
