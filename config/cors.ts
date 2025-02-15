620ffed1b58b5c8ebd986c7937478d81500fdc0d
wsameer <dev.wsameer@gmail.com>
2025-02-15 19:27:50 -0400

    origin sorted

--- config/cors.ts
+++ config/cors.ts
@@ -8,7 +8,7 @@ import { defineConfig } from '@adonisjs/cors'
  */
 const corsConfig = defineConfig({
   enabled: true,
-  origin: process.env.NODE_ENV === 'production' ? process.env.FRONTEND_URL : '*',
+  origin: true,
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
   headers: true,
   exposeHeaders: [],
