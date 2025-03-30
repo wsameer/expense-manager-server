307bf58a04777d89c3871769cc8adf571f41c6f7
wsameer <dev.wsameer@gmail.com>
2025-03-29 23:02:06 -0400

    alignment chagnes

--- app/controllers/user_profiles_controller.ts
+++ app/controllers/user_profiles_controller.ts
@@ -1,5 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class UserProfilesController {
-  async index({}: HttpContext) {}
+  async index({}: HttpContext) {
+    
+  }
 }
